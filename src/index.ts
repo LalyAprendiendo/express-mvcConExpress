693456d02e088210402c14cd8223ba5adc3e7b8d
Tomas <arnotomas1@gmail.com>
2024-07-29 18:58:30 -0300

    estructura basica

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { json } from 'express';
+
+const PORT = 8080;
+const app = express();
+
+// MIDDLEWARES
+
+
+
+// ROUTES 
+
+
+
+// SERVER 
+
+app.listen(PORT, () => {
+	console.log('Server listening on port', PORT);
+});
